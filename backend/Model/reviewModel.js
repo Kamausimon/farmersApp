8ae5cb123039dca747495e38cb7bf61f43bc7446
kamau simon <kamausimon217@gmail.com>
2024-02-26 03:23:20 +0300

    review Schema

---
+++ backend/Model/reviewModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.schema({
+  review: {
+    type: "String",
+    required: [true, "Provide a review on the product"],
+  },
+  rating: {
+    type: "Number",
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  //review must be connected to the product
+  product: {
+    type: mongoose.schema.ObjectId,
+    ref: "Product",
+    required: [true, "review must belong to a product"],
+  },
+  //review must be connected to the user
+  user: {
+    type: mongoose.schema.ObjectId,
+    ref: "User",
+    required: [true, "review must belong to a user"],
+  },
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
