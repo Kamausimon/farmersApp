a8e0df57f9feefa28925488d7700ae9e8b5da1d0
kamau simon <kamausimon217@gmail.com>
2024-03-06 02:07:23 +0300

    update

--- backend/Controller/messageController.js
+++ backend/Controller/messageController.js
@@ -1,10 +1,39 @@
 const Messages = require("./messageController");
 const AppError = require("../utils/AppError");
 
-exports.getAllMessages = async (req, res, next) => {
-  const messages = await Messages.find({ user: userId });
+const fetchMessages = async (Id, res) => {
+  try {
+    const messages = await Message.find(Id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        messages,
+      },
+    });
+  } catch (err) {
+    console.log("There was an error");
+    next(new AppError("There was an error fetching all the messages", 400));
+  }
 };
 
-exports.sendMessage = async (req, res, next) => {};
+exports.getAdminMessages = async (req, res, next) => {
+  const Id = { admin: Admin };
+  await fetchMessages(Id, res);
+};
+
+exports.getUserMessages = async (req, res, next) => {
+  const Id = { user: UserId };
+  await fetchMessages(Id, res);
+};
+
+exports.getFarmerMessages = async (req, res, next) => {
+  const Id = { farmer: farmerId };
+  await fetchMessages(Id, res);
+};
+
+exports.sendMessage = async (req, res, next) => {
+  const 
+};
 
 exports.getOneMessage = async (req, res, next) => {};
--- backend/Routes/messageRouter.js
+++ backend/Routes/messageRouter.js
@@ -3,7 +3,10 @@ const messageController = require("../Controller/messageController");
 
 const router = express.Router();
 
-router.route("/message").get(messageController.getAllMessages);
+router.route("/message").get(messageController.getAdminMessages);
+router.route("/message").get(messageController.getUserMessages);
+router.route("/message").get(messageController.getFarmerMessages);
+
 router.post("/message").post(messageController.sendMessage);
 
 //get message by id
