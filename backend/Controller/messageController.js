23c5034d4a4ef784243af28f426a9d51c7c7f417
kamau simon <kamausimon217@gmail.com>
2024-03-06 20:30:54 +0300

    message controller updated

--- backend/Controller/messageController.js
+++ backend/Controller/messageController.js
@@ -1,5 +1,23 @@
 const Messages = require("./messageController");
 const AppError = require("../utils/AppError");
+const app = require("../App");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+//store user sockets
+const userSockets = {};
+
+//check the connectivity
+io.on("connection", (socket) => {
+  //when a user connects store their socket
+  socket.on("userConnected", (Id) => {
+    userSockets[Id] = socket.id;
+  });
+  //when user disconects remove the socket
+  socket.on("disconnect", () => {
+    delete userSockets[socket.id];
+  });
+});
 
 const fetchMessages = async (Id, res) => {
   try {
@@ -32,8 +50,38 @@ exports.getFarmerMessages = async (req, res, next) => {
   await fetchMessages(Id, res);
 };
 
-exports.sendMessage = async (req, res, next) => {
-  const 
+exports.newMessage = async (req, res, next) => {
+  try {
+    //extract info from the request body
+    const { Id, content } = req.body;
+
+    //use that info to create a new message body and await as you save to the db
+    const newMessage = await Message.create({ Id, content });
+
+    if (userSockets[Id]) {
+      io.to(userSockets[Id]).emit("message", newMessage);
+    }
+
+    req.message = newMessage;
+    next();
+  } catch (err) {
+    console.log("There was an error creating the message", err);
+    next(new AppError("An error occurred while creating the message", 400));
+  }
 };
 
-exports.getOneMessage = async (req, res, next) => {};
+exports.getOneMessage = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const message = await Message.findById(id);
+    if (!message) {
+      return res.status(400).send({ error: "Message not found" });
+    }
+    res.message = message;
+    next();
+  } catch (err) {
+    console.log("There was an error fetching the message", err);
+    next(new AppError(" an error occurred while fetching the message", 404));
+  }
+};
--- backend/Routes/messageRouter.js
+++ backend/Routes/messageRouter.js
@@ -7,7 +7,7 @@ router.route("/message").get(messageController.getAdminMessages);
 router.route("/message").get(messageController.getUserMessages);
 router.route("/message").get(messageController.getFarmerMessages);
 
-router.post("/message").post(messageController.sendMessage);
+router.post("/message").post(messageController.newMessage);
 
 //get message by id
 router.route("/message/:id").get(messageController.getOneMessage);
